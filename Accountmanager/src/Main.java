ff103367ec672a3e2ad986bf2b35cdd20000b4c6
maxmustermann4@gmx.net <Blackjack38!>
2017-07-19 19:24:34 +0200

    Integrierte Main

--- Accountmanager/src/Main.java
+++ Accountmanager/src/Main.java
@@ -1,29 +1,101 @@
-import EmpDatabase.Login;
+import java.sql.*;
 
 import Employee.InternalEmp;
+
+import GUI.Login;
 import StorageController.JDBCController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-import java.sql.SQLException;
-import java.util.Scanner;
+import static javafx.application.Application.launch;
 
-/**
- * Created by Ben on 13.12.2016.
- */
+
 public class Main
 {
 
-    public static void main(String [] args) throws SQLException
-
+    public static void main(String[] args)
     {
+
+
+        /** public void start(Stage primaryStage) throws SQLException
+         {
+
+         primaryStage.setTitle("Hauptbildschirm");
+         GridPane newGrid = new GridPane();
+         Scene newScene = new Scene(newGrid);
+         primaryStage.setScene(newScene);
+         primaryStage.show();
+         primaryStage.setFullScreen(false);
+
+         Stage TestLogin = new Stage();
+         LoginGUI TestGUI = new LoginGUI();
+
+         TestGUI.start(TestLogin);
+         TestLogin.setFullScreen(false);
+         TestLogin.show();
+
+         InternalEmp TMP = new InternalEmp();
+         InternalEmp Karl = new InternalEmp("Karl", "Mustermann", "KarlMustermann@gmx.de");
+         InternalEmp Maxi = new InternalEmp("Maxi", "Mustermann", "MaxiMustermann@gmx.de");
+         InternalEmp Kalle = new InternalEmp("Kalle", "Mustermann", "KalleMustermann@gmx.de");
+         **/
+
+        InternalEmp Karl = new InternalEmp("Karl", "Nett", "dd@dd.de");
+
+        Karl.setAccessLevel(2);
+        Karl.setPassword("Blackjack");
+        Karl.setSalary(2000);
+        Karl.setOnStatus(true);
+
+        InternalEmp _tmp = new InternalEmp();
+
+
         JDBCController Controller = new JDBCController();
 
 
-        Controller.loadPersonal();
+        try
+        {
 
+            Controller.showEmployee();
+            _tmp = Controller.loadEmployee("MustermannKalle");
+        }
 
+        catch (SQLException e)
+        {}
+
+        System.out.println(_tmp.getFirstName());
+        System.out.println(_tmp.getEmpID());
+
+        /**
+
+         InternalEmp _tmp = null;
+
+         Connection conn = null;
+
+         try
+         {
+         Controller.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/accountdb","root", "Blackjack");
+
+         Statement stmt = Controller.connection.createStatement();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM accountdb.personalintern");
+
+         while (rs.next()) System.out.println(rs.getString("firstName"));
+
+         }
+
+         catch (SQLException e)
+         {
+         e.printStackTrace();
+         }
+
+         **/
 
     }
 
-
-
 }
+
+
+
+
